317a2ad739affa064169824679d0a877664f65dd
Milad <khademmilad@gmail.com>
2023-06-15 00:32:02 +0200

    some change

--- src/code_editor/src/ProfilePage.js
+++ src/code_editor/src/ProfilePage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
 
 function ProfilePage() {
   const [posts, setPosts] = useState([]);
@@ -11,24 +12,25 @@ function ProfilePage() {
     try {
       const userId = 1;
       const response = await fetch(`http://127.0.0.1:8000/api/posts/user/1/`);
-      const responseData = await response.text();
-      console.log(responseData); // Log the response for inspection
-      const data = JSON.parse(responseData);
-      setPosts(data);
-      console.log('salam', data)
+      const responseData = await response.json();
+      setPosts(responseData);
     } catch (error) {
-      // console.error('Error fetching posts:', error);
+      console.error('Error fetching posts:', error);
     }
   };
 
   return (
-    <div>
-      <h1>Profile Page</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+    <div className="container">
+      <Navbar />
+
+      <div className="profile-content">
+        <h1>Profile Page</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
